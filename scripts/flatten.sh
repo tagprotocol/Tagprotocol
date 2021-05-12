0518819596f25191a13f04d396e82d0b8dcabb91
TAG Worker <80066266+TAGFOUNDATION@users.noreply.github.com>
2021-05-12 18:36:03 +0400

    Delete scripts directory

--- scripts/flatten.sh
+++
@@ -1,3 +0,0 @@
-npx hardhat flatten contracts/protocol/TagRates.sol > flatten/TagRates.sol
-npx hardhat flatten contracts/protocol/TagTreasury.sol > flatten/TagTreasury.sol
-npx hardhat flatten contracts/token/TagNFT.sol > flatten/TagNFT.sol
